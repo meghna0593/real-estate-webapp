890d7266f8fdb97c134513e5e673b14d3b1475b6
Harry Sohal <meghna0593@gmail.com>
2024-05-31 18:45:52 -0700

    configure django logging

--- real_estate/settings/base.py
+++ real_estate/settings/base.py
@@ -124,3 +124,52 @@ MEDIA_ROOT = BASE_DIR / "mediafiles"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import logging
+import logging.config
+
+from django.utils.log import DEFAULT_LOGGING
+
+logger= logging.getLogger(__name__)
+
+LOG_LEVEL="INFO"
+
+logging.config.dictConfig({
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters":{ # Formatters define the layout of the log messages
+        "console":{
+            "format":"%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
+            
+        },
+        "file": {"format":"%(asctime)s %(name)-12s %(levelname)-8s %(message)s"},
+        "django.server": DEFAULT_LOGGING["formatters"]["django.server"],
+    },
+    "handlers":{ # Handlers determine where the log messages go (console, file, etc.)
+        "console":{
+            "class":"logging.StreamHandler",
+            "formatter": "console"
+        },
+        "file":{
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "formatter": "file",
+            "filename":"logs/real_estate.log", # file is created automatically
+
+        },
+        "django.server": DEFAULT_LOGGING["handlers"]["django.server"],
+    },
+    "loggers":{ # Loggers define which log messages are handled by which handlers
+        "":{
+            "level": "INFO",
+            "handlers": ["console", "file"],
+            "propagate": False
+        },
+        "apps": {
+            "level": "INFO",
+            "handlers": ["console"],
+            "propagate": False
+        },
+        "django.server": DEFAULT_LOGGING["loggers"]["django.server"],
+    }
+})

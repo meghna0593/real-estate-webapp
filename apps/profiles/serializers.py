45f7001c258f16b631b4a1b2865149edca6ebf8a
Harry Sohal <meghna0593@gmail.com>
2024-06-03 16:35:09 -0700

    Update Profile views, serializers, urls

---
+++ apps/profiles/serializers.py
@@ -0,0 +1,79 @@
+from apps.ratings.serializers import RatingSerializer
+from django_countries.serializer_fields import CountryField
+from .models import Profile
+from rest_framework import serializers
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source="user.username")
+    first_name = serializers.CharField(source="user.first_name")
+    last_name = serializers.CharField(source="user.last_name")
+    email = serializers.EmailField(source="user.email")
+    full_name = serializers.SerializerMethodField(read_only=True)
+    country = CountryField(name_only=True)
+    reviews = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Profile
+        fields = [
+            "username",
+            "first_name",
+            "last_name",
+            "full_name",
+            "email",
+            "phone_number",
+            "id",
+            "about_me",
+            "gender",
+            "license",
+            "profile_photo",
+            "country",
+            "city",
+            "is_buyer",
+            "is_seller",
+            "is_agent",
+            "rating",
+            "num_reviews",
+            "reviews",
+        ]
+
+    def get_full_name(self, obj):
+        first_name = obj.user.first_name.title()
+        last_name = obj.user.last_name.title()
+        return f"{first_name} {last_name}"
+
+    def get_reviews(self, obj):
+        reviews = obj.agent_review.all()
+        serializer = RatingSerializer(reviews, many=True)
+        return serializer.data
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        if instance.top_agent:
+            representation["top_agent"] = True
+        return representation
+
+
+class UpdateProfileSerializer(serializers.ModelSerializer):
+    country = CountryField(name_only=True)
+
+    class Meta:
+        model = Profile
+        fields = [
+            "phone_number",
+            "about_me",
+            "gender",
+            "license",
+            "profile_photo",
+            "country",
+            "city",
+            "is_buyer",
+            "is_seller",
+            "is_agent",
+        ]
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        if instance.top_agent:
+            representation["top_agent"] = True
+        return representation
--- apps/profiles/views.py
+++ apps/profiles/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from .exceptions import NotYourProfile, ProfileNotFound
+from .models import Profile
+from .renderers import ProfileJSONRenderer
+from .serializers import ProfileSerializer, UpdateProfileSerializer
 
-# Create your views here.
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class AgentListAPIView(generics.ListAPIView):  # class based views
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Profile.objects.filter(is_agent=True)
+    serializer_class = ProfileSerializer
+
+
+class TopAgentsListAPIView(generics.ListAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Profile.objects.filter(top_agent=True)
+    serializer_class = ProfileSerializer
+
+
+class GetProfileAPIView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    renderer_classes = [ProfileJSONRenderer]
+
+    def get(self, request):
+        user = self.request.user
+        user_profile = Profile.objects.get(user=user)
+        serializer = ProfileSerializer(user_profile, context={"request": request})
+
+        return Response(serializer.data, status=status.HTTP_200_OK)  # will be under the profile namespace
+
+
+class UpdateProfileAPIView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    renderer_classes = [ProfileJSONRenderer]
+    serializer_class = UpdateProfileSerializer
+
+    def patch(self, request, username):
+        try:
+            Profile.objects.get(user__username=username)
+        except Profile.DoesNotExist:
+            raise ProfileNotFound
+
+        user_name = request.user.username
+        if user_name != username:
+            raise NotYourProfile
+
+        data = request.data
+        serializer = UpdateProfileSerializer(instance=request.user.profile, data=data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.validated_data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_200_OK)
